657dc7f81071e3abb522c24e648b5fc38f35a600
ruruisu <zaddozgl@gmail.com>
2022-03-28 18:14:51 -0600

    random anime implementation

--- main.js
+++ main.js
@@ -7,16 +7,36 @@ document.addEventListener("DOMContentLoaded", async () => {
       leftAnime = data;
    
     let p = document.getElementById("left_popularity");
-    p.innerHTML = leftAnime._popularityRanking;
+    p.innerHTML = "#" + leftAnime._popularityRanking;
     console.log(leftAnime._popularityRanking);
+
+    let t = document.getElementById("left_title");
+    t.innerHTML = leftAnime._name;
+    console.log(leftAnime._title);
+
+    let i = document.getElementById("left_imageurl");
+    i.src = leftAnime._pictureUrl;
+    console.log(leftAnime._pictureUrl);
+
     });
-    let rightAnime = animeList.assignAnime();
+    let rightAnime;
     animeList.assignAnime().then((data) => {
       rightAnime = data;
-      console.log(rightAnime);
+      
+      // let p = document.getElementById("right_popularity");
+      // p.innerHTML = "#" + rightAnime._popularityRanking;
+      // console.log(rightAnime._popularityRanking);
+  
+      let t = document.getElementById("right_title");
+      t.innerHTML = rightAnime._name;
+      console.log(rightAnime._title);
+  
+      let i = document.getElementById("right_imageurl");
+      i.src = rightAnime._pictureUrl;
+      console.log(rightAnime._pictureUrl);
+  
     });
-    
-    let r;
+
   } catch (e) {
     console.log("Error: ");
     console.log(e);
